85c93784bc3022a5f0c9f2742632edac36eb5c6f
Kanan Farzaliyev <kanan.farzali@gmail.com>
2024-06-12 19:13:58 +0200

    building the app layout

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,13 +1,15 @@
-function CartOverview() {
+import { Link } from "react-router-dom";
+
+const CartOverview = () => {
   return (
     <div>
       <p>
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
-      <a href="#">Open cart &rarr;</a>
+      <Link to="/cart">Open cart &rarr;</Link>
     </div>
   );
-}
+};
 
 export default CartOverview;
