ccc00705ef3e95e13b6da5b9799c1669f7165f99
Kanan Farzaliyev <kanan.farzali@gmail.com>
2024-06-13 10:55:46 +0200

    displaying a loading indicator

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
-import Menu, { loader as MenuLoader } from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
-        loader: MenuLoader,
+        loader: menuLoader,
       },
       {
         path: "/cart",
