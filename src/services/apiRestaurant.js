0ee88f4319055c14216fc9c333a8ac0af13c1492
Kanan Farzaliyev <kanan.farzali@gmail.com>
2024-06-13 10:40:55 +0200

    fetching data with react router loaders: pizza menu

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -1,6 +1,6 @@
 const API_URL = "https://react-fast-pizza-api.onrender.com/api";
 
-export async function getMenu() {
+export const getMenu = async () => {
   const res = await fetch(`${API_URL}/menu`);
 
   // fetch won't throw error on 400 errors (e.g. when URL is wrong), so we need to do it manually. This will then go into the catch block, where the message is set
@@ -8,7 +8,7 @@ export async function getMenu() {
 
   const { data } = await res.json();
   return data;
-}
+};
 
 export async function getOrder(id) {
   const res = await fetch(`${API_URL}/order/${id}`);
