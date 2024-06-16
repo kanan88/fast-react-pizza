dfef2604d32ddd06d2cfd808fa9f032fd9adbfd1
Kanan Farzali <kanan.farzali@gmail.com>
2024-06-16 20:35:15 +0200

    styling menu

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -24,7 +24,7 @@ const CreateUser = () => {
 
       {username !== "" && (
         <div>
-          <Button>Start ordering</Button>
+          <Button type="primary">Start ordering</Button>
         </div>
       )}
     </form>
