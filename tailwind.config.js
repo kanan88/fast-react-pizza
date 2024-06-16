b3c4e262692a4132bf6ee4b9ba3f47f67607e82c
Kanan Farzali <kanan.farzali@gmail.com>
2024-06-16 19:51:55 +0200

    configuring tailwind: custom font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+/* eslint-disable import/no-anonymous-default-export */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+
+    extend: {
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
